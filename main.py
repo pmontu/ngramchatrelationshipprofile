ad7a74049e6d07a2727508a0abb3ec5a55b7bfee
Manoj <pmontu@gmail.com>
2013-11-05 07:02:58 +0530

    ranks

--- main.py
+++ main.py
@@ -15,20 +15,38 @@ words[1].sort()
 #print words[0]
 #print words[1]
 
-previous = words[0][0]
-c=1
-i=1
-l=len(words[0])
-cloud = []
-while i<l:
-    current = words[0][i]
-    if previous == current:
-        c+=1
-    else:
-        cloud.append([previous,c])
-        c=1
-        previous = current
-    i+=1
-print cloud
+def makecloud(words):
+    previous = words[0]
+    c=1
+    i=1
+    l=len(words)
+    cloud = []
+    while i<l:
+        current = words[i]
+        if previous == current:
+            c+=1
+        else:
+            cloud.append([previous,c])
+            c=1
+            previous = current
+        i+=1
+    return sorted(cloud, key=lambda student: student[1], reverse=True)
+
+cloud1=makecloud(words[0])
+cloud2=makecloud(words[1])
+
+def makeranks(cloud):
+    l = len(cloud)
+    i=0
+    while i<l:
+        cloud[i].append(i+1)
+        i+=1
+    return cloud
+
+cloud1=makeranks(cloud1)
+cloud2=makeranks(cloud2)
+
+print cloud1
+print cloud2
 
 
